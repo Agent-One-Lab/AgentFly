62f05497e4abcf3658ac520b40a5958e7f551bb8
Rifo Genadi <rifoagenadi@gmail.com>
2025-07-24 11:27:21 +0000

    update enroot test

--- agents/tests/unit/envs/test_enroot.py
+++ agents/tests/unit/envs/test_enroot.py
@@ -4,7 +4,7 @@ def test_enroot_client():
     client = from_env()
     assert client.ping()
     container = client.containers.run("nvidia/cuda:11.7.1-devel-ubuntu20.04", "sleep infinity", detach=True)
-    assert container.status == "running"
+    # assert container.status == "running"
     assert container.attrs["State"]["Status"] == "running"
     assert container.attrs["State"]["Running"] == True
 
